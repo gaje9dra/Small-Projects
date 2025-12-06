e9bc83c8b5e98bd8b28428cd4fd4abdff45d0e93
gaje9dra <126646046+gaje9dra@users.noreply.github.com>
2025-12-06 12:08:27 +0530

    Add files via upload

---
+++ oddEven.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class oddEven{
+public static void main(String[] args){
+Scanner sc = new Scanner(System.in);
+int number = sc.nextInt();
+if(number%2==0){
+System.out.println("This is a even number");
+}else{
+System.out.println("This is an odd number");
+}}}
