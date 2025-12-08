2c167cb2f1eea13c7cefe2493fb8dc06ed17a243
gaje9dra <126646046+gaje9dra@users.noreply.github.com>
2025-12-08 10:56:06 +0530

    Add files via upload
    basic loops  practice

---
+++ practiceLoops.java
@@ -0,0 +1,18 @@
+public class practiceLoops{
+public static void main(String[] args){
+int i=1;
+for(int j=1;j<=10;j++){
+System.out.println("for" +j);
+}
+
+while(i<=10){
+System.out.println("while"+i);
+i++;
+}
+
+int k=1;
+do{
+System.out.println("do while"+k);
+k++;
+}while(k<=10);
+}}
